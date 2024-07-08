22cb713d5a65306bf36d73e2c18e3587f197e763
rodrigoalb <rodrigoalba-@outlook.com>
2024-07-08 13:06:45 -0300

    ORM Mapping, database seeding

--- backend/src/main/java/com/example/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/example/dscatalog/services/CategoryService.java
@@ -8,7 +8,6 @@ import com.example.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
